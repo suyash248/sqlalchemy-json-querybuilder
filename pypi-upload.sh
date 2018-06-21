7e763d30b65605d102e4343eb353ce65297d3eef
Suyash Soni <suyash.soni248@gmail.com>
2018-06-21 10:53:45 +0530

    upload script

--- pypi-upload.sh
+++ pypi-upload.sh
@@ -1,1 +1,5 @@
 #!/usr/bin/env bash
+
+rm -rf dist build sqlalchemy_json_querybuilder.egg-info
+python3 setup.py sdist bdist_wheel
+twine upload --repository-url https://test.pypi.org/legacy/ dist/*
