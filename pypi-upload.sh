dcea1c6964869e72e8b4fb7a7270839fd359d676
Suyash Soni <suyash.soni248@gmail.com>
2018-06-23 12:49:11 +0530

    Updated setup tool

--- pypi-upload.sh
+++ pypi-upload.sh
@@ -4,7 +4,7 @@ rm -rf dist build sqlalchemy_json_querybuilder.egg-info
 python3 setup.py sdist bdist_wheel
 
 # For test.pypi
-#twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 
 # For pypi
 twine upload dist/*
